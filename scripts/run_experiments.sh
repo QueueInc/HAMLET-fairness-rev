2cc27ba7b1d86b5feda736c815566d1ca8330371
Joseph Giovanelli <j.giovanelli@unibo.it>
2024-06-19 17:17:46 +0200

    Add results pushing

--- scripts/run_experiments.sh
+++ scripts/run_experiments.sh
@@ -5,6 +5,16 @@ python automl/run_hamlet.py --workspace $1/hamlet/baseline --metric balanced_acc
 python automl/run_hamlet.py --workspace $1/hamlet/pkb --metric balanced_accuracy --fair_metric $4 --mining_target $5 --mode max --batch_size 500 --time_budget 7200 --version $2 --iterations 1 --volume $3 --kb /home/HAMLET-fairness/resources/pkb.txt
 python automl/run_hamlet.py --workspace $1/hamlet/ika --metric balanced_accuracy --fair_metric $4 --mining_target $5 --mode max --batch_size 125 --time_budget 1800 --version $2 --iterations 4 --volume $3 --kb /home/HAMLET-fairness/resources/kb.txt
 python automl/run_hamlet.py --workspace $1/hamlet/pkb_ika --metric balanced_accuracy --fair_metric $4 --mining_target $5 --mode max --batch_size 125 --time_budget 1800 --version $2 --iterations 4 --volume $3 --kb /home/HAMLET-fairness/resources/pkb.txt
+cd ..
+git config --global user.email "j.giovanelli@unibo.it"
+git config --global user.name "Joseph Giovanelli"
+git clone https://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@github.com/QueueInc/HAMLET-fairness-results.git
+cd HAMLET-fairness-results
+cp -r $1 "./$2-$4-$5"
+git add .
+git commit -m "new results in $2-$4-$5"
+git push
+
 # python automl/run_comparison.py --tool auto-sklearn --budget 3600 --output_folder $1/auto_sklearn
 # python automl/run_comparison.py --tool h2o --budget 3600 --output_folder $1/h2o
 # python automl/post_processor/etl.py --path $1 --input-folder hamlet --output-folder hamlet --budget 500
