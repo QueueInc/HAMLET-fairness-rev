2340409ac8e7d787277769302e9a53d6275972e5
Giuseppe Pisano <giuseppe.pisano3@studio.unibo.it>
2024-06-21 16:10:17 +0200

    cleaning

--- Dockerfile
+++ Dockerfile
@@ -5,11 +5,6 @@ COPY requirements.txt requirements.txt
 RUN pip install --upgrade pip && \
     pip install black && \
     pip install --no-cache-dir --upgrade -r /requirements.txt && \
-    pip install requests && \
-    pip install tabulate && \
-    pip install future && \
-    pip uninstall h2o && \
-    pip install -f http://h2o-release.s3.amazonaws.com/h2o/latest_stable_Py.html h2o && \
     rm requirements.txt
 RUN cd home && mkdir HAMLET-fairness
 WORKDIR /home/HAMLET-fairness
--- scripts/run_experiments.sh
+++ scripts/run_experiments.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 echo Running EXPERIMENTS
 wget https://github.com/QueueInc/HAMLET/releases/download/$2/hamlet-$2-all.jar
-python automl/run_hamlet.py --workspace $1/hamlet/baseline --metric balanced_accuracy --fair_metric $4 --mining_target $5 --mode max --batch_size 500 --time_budget 60 --version $2 --iterations 1 --volume $3 --kb /home/HAMLET-fairness/resources/kb.txt
-python automl/run_hamlet.py --workspace $1/hamlet/pkb --metric balanced_accuracy --fair_metric $4 --mining_target $5 --mode max --batch_size 500 --time_budget 60 --version $2 --iterations 1 --volume $3 --kb /home/HAMLET-fairness/resources/pkb.txt
-python automl/run_hamlet.py --workspace $1/hamlet/ika --metric balanced_accuracy --fair_metric $4 --mining_target $5 --mode max --batch_size 125 --time_budget 60 --version $2 --iterations 4 --volume $3 --kb /home/HAMLET-fairness/resources/kb.txt
-python automl/run_hamlet.py --workspace $1/hamlet/pkb_ika --metric balanced_accuracy --fair_metric $4 --mining_target $5 --mode max --batch_size 125 --time_budget 60 --version $2 --iterations 4 --volume $3 --kb /home/HAMLET-fairness/resources/pkb.txt
+python automl/run_hamlet.py --workspace $1/hamlet/baseline --metric balanced_accuracy --fair_metric $4 --mining_target $6 --mode max --batch_size 500 --time_budget 7200 --version $2 --iterations 1 --volume $3 --kb /home/HAMLET-fairness/resources/kb.txt
+python automl/run_hamlet.py --workspace $1/hamlet/pkb --metric balanced_accuracy --fair_metric $4 --mining_target $6 --mode max --batch_size 500 --time_budget 7200 --version $2 --iterations 1 --volume $3 --kb /home/HAMLET-fairness/resources/pkb.txt
+python automl/run_hamlet.py --workspace $1/hamlet/ika --metric balanced_accuracy --fair_metric $4 --mining_target $6 --mode max --batch_size 125 --time_budget 1800 --version $2 --iterations 4 --volume $3 --kb /home/HAMLET-fairness/resources/kb.txt
+python automl/run_hamlet.py --workspace $1/hamlet/pkb_ika --metric balanced_accuracy --fair_metric $4 --mining_target $6 --mode max --batch_size 125 --time_budget 1800 --version $2 --iterations 4 --volume $3 --kb /home/HAMLET-fairness/resources/pkb.txt
 cd ..
 git config --global user.email "j.giovanelli@unibo.it"
 git config --global user.name "Joseph Giovanelli"
-git clone "https://$6@github.com/QueueInc/HAMLET-fairness-results.git"
+git clone "https://$5@github.com/QueueInc/HAMLET-fairness-results.git"
 cd HAMLET-fairness-results
-cp -r $1 "./$2-$4-$5-test"
+cp -r $1 "./$2-$4-$6"
 git add .
-git commit -m "new results in $2-$4-$5"
+git commit -m "new results in $2-$4-$6"
 git push
 
 # python automl/run_comparison.py --tool auto-sklearn --budget 3600 --output_folder $1/auto_sklearn
